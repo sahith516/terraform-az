43699b6aac4c8ebfce495d2b4a1cdcbb91d7a8ed
sahith516 <sahith516@gmail.com>
2026-02-13 12:34:04 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tf-backend-rg"
-    storage_account_name = "tfbackendstorage123"
+    resource_group_name  = "sahith"
+    storage_account_name = "sahithstorage516"
     container_name       = "tfstate"
     key                  = "canadacentral-vm.tfstate"
   }
